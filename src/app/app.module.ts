ff557d147dee6e8bf0067263ab3627c27f844b8b
Reinner Daza Leiva <reinnerdazaleiva@MacBook-Pro-de-Reinner.local>
2021-04-26 21:29:02 -0500

    Storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { IonicStorageModule } from '@ionic/storage-angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -16,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    IonicStorageModule.forRoot(),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/pages/splash/splash.page.ts
+++ src/app/pages/splash/splash.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { MESSAGE } from 'src/app/enums/e.message';
 import { END_POINTS, HTTP_METHODS } from 'src/app/enums/e.request';
 import { IQuestion } from 'src/app/interface/i.question';
 import { RequestService } from 'src/app/services/request.service';
+import { StorageService } from 'src/app/services/storage.service';
 import { PresentersService } from 'src/app/services/utils/presenters.service';
 
 @Component({
@@ -14,18 +16,35 @@ export class SplashPage implements OnInit {
 
   constructor(
     private _request: RequestService,
+    private _storage: StorageService,
+    private _navCtrl: NavController,
     private _presenters: PresentersService) { }
 
   ngOnInit() {
-    this.makeRequest();
+    this.managerData();
+  }
+
+  private async managerData() {
+    const listResult: IQuestion[] = await this._storage.get('question')
+
+    if (listResult.length == 0) {
+      this.makeRequest();
+    } else {
+      console.log(listResult);
+      setTimeout(() => {
+        this.goToHomApp();
+      }, 500)
+    }
   }
 
   private makeRequest() {
     this._presenters.showLoading();
     this._request.makeRequest(HTTP_METHODS.GET, END_POINTS.API, null, '?amount=10&difficulty=hard&type=boolean').then((response) => {
-      if(response.status == 200){
+      if (response.status == 200) {
         let listResult: IQuestion[] = response.body.results;
-      }else{
+        this._storage.set('question', listResult);
+        this.goToHomApp();
+      } else {
         this._presenters.showToast(MESSAGE.NOT_STATUS_CODE);
       }
     }).catch((err) => {
@@ -35,4 +54,10 @@ export class SplashPage implements OnInit {
     })
   }
 
+  private goToHomApp() {
+    this._navCtrl.navigateRoot('/home', {
+      animated: true,
+      animationDirection: "forward",
+    });
+  }
 }
